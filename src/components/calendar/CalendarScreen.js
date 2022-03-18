bfb0a41d3dbf6dda7d5f61fd70fc5b3e241ec23f
Alejandro Pedrosa <alejandro.pedrosa@hotmail.com>
2022-03-18 15:45:38 -0300

    Calendar styles

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+
+import "moment/locale/es";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import { NavBar } from "../ui/NavBar";
+import { messages } from "../../helpers/calendar-messages-es";
+
+moment.locale("es");
+
+const localizer = momentLocalizer(moment);
+const events = [
+  {
+    title: "Cumpleaños del jefe",
+    start: moment().toDate(),
+    end: moment().add(2, "hours").toDate(),
+    bgcolor: "#fafafa",
+  },
+];
 
 export const CalendarScreen = () => {
   return (
-    <div>
-      <NavBar />
+    <div className="calendar-container">
+      <div className="calendar-screen">
+        <NavBar />
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          messages={messages}
+        />
+      </div>
     </div>
   );
 };
