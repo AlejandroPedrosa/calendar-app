ba35f02890a5005b178ba6ad0399b3d6e2db8b1d
Alejandro Pedrosa <alejandro.pedrosa@hotmail.com>
2022-03-26 16:10:13 -0300

    Login con db y auth

--- src/types/types.js
+++ src/types/types.js
@@ -7,4 +7,13 @@ export const types = {
   eventClearActiveEvent: "[EVENT] Clear active event",
   eventUpdated: "[EVENT] Event updated",
   eventDeleted: "[EVENT] Event deleted",
+
+  authChecking: "[AUTH] Checking",
+  authChekingFinish: "[AUTH] Cheking finish login state",
+  authStartLogin: "[AUTH] Start login",
+  authLogin: "[AUTH] Login",
+  authStartRegister: "[AUTH] Start register",
+  authRegister: "[AUTH] Register",
+  authStartTokenRenew: "[AUTH] Start token renew",
+  authLogout: "[AUTH] Logout",
 };
